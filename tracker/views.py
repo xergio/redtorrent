89c21078439cf3d55a3e999ee5cd477e1baa2e02
Sergio Álvarez <xergio@gmail.com>
2012-04-06 13:12:45 +0200

    basic handle

--- tracker/views.py
+++ tracker/views.py
@@ -1,10 +1,12 @@
 
 from django.shortcuts import render_to_response
+from django.http import HttpResponse
 
 import sys
 import socket
-import benc
+import bencode
 import redis
+import struct
 
 """
 /announce?
@@ -22,149 +24,90 @@ event=started
 
 /scrape?
 info_hash=gK%91d%e0%ec%fc%c0G%c1%0a%9bD8%85%a9%99%88%27%da
+
+[06/Apr/2012 06:08:29] "GET /announce?info_hash=7%cc%08%1fG%60%a6%ab%05%1d%b8%d6%fa%d6%cd%2b%a1gl%98&peer_id=-TR2500-1u2n01ylhvvp&port=51413&uploaded=0&downloaded=0&left=0&numwant=80&key=90ecacb&compact=1&supportcrypto=1&event=started HTTP/1.1" 200 25
+[06/Apr/2012 06:09:30] "GET /announce?info_hash=7%cc%08%1fG%60%a6%ab%05%1d%b8%d6%fa%d6%cd%2b%a1gl%98&peer_id=-TR2500-1u2n01ylhvvp&port=51413&uploaded=0&downloaded=0&left=0&numwant=80&key=90ecacb&compact=1&supportcrypto=1 HTTP/1.1" 200 25
 """
 
 def announce(request):
-    #r = redis.StrictRedis(host='localhost')
-    try:
-	    info = {}
+	try:
+		r = redis.StrictRedis(host='localhost')
+		info = {}
 
-	    info['ip'] = request.GET.get('ip') or request.META.get('REMOTE_ADDR')
-	    try:
-	        socket.gethostbyname(info['ip'])
-	    except:
-	        raise Exception("unable to resolve host name '%s'." % info['ip'])
+		info_hash = request.GET.get('info_hash', '')
+		if len(info_hash) < 20:
+			raise Exception("problem with info hash.")
+		info['info_hash'] = info_hash
 
-	    for key in ['uploaded', 'downloaded', 'port', 'left']:
-	        if request.GET.has_key(key):
-	            try:
-	                info[key] = int(request.GET[key])
-	            except ValueError:
-	                raise Exception("argument '%s' has an incorrect type." % key)
-	        else:
-	            raise Exception("argument '%s' not specified." % key)
+		if not request.GET.get('peer_id', ''):
+			raise Exception('problem with peer id.')
+		info['peer_id'] = request.GET['peer_id']
 
-	    event = request.GET.get('event', '')
-	    if event not in ['started', 'completed', 'stopped'] and len(event.strip()) > 0:
-	        raise Exception("event '%s' is invalid" % event)
+		info['ip'] = request.GET.get('ip') or request.META.get('REMOTE_ADDR')
+		try:
+			socket.gethostbyname(info['ip'])
+		except:
+			raise Exception("unable to resolve host name '%s'." % info['ip'])
 
-	    # is the announce method allowed ?
-	    if REQUIRE_ANNOUNCE_PROTOCOL == 'no_peer_id':
-	        if not request.GET.get('compact') and not request.GET.get('no_peer_id'):
-	            return _fail("standard announces not allowed; use no_peer_id or compact option")
-	    elif REQUIRE_ANNOUNCE_PROTOCOL == 'compact':
-	        if not request.GET.get('compact'):
-	            return _fail("tracker requires use of compact option")
+		for key in ['uploaded', 'downloaded', 'port', 'left']:
+			if request.GET.has_key(key):
+				try:
+					info[key] = int(request.GET[key])
+				except ValueError:
+					raise Exception("argument '%s' has an incorrect type." % key)
+			else:
+				raise Exception("argument '%s' not specified." % key)
 
-	    info_hash = request.GET.get('info_hash', '')
-	    if len(info_hash) < 20 or not request.GET.get('peer_id'):
-	        return _fail("invalid request")
-	    try:
-	        info_hash = info_hash.encode('iso-8859-1').encode('hex')
-	    except:
-	        return _fail("invalid request")
-	    args['peer_id'] = request.GET['peer_id']
-	    torrent_id = Torrent.objects.filter(info_hash=info_hash).values('id')
-	    if not torrent_id:
-	        return _fail("no such torrent")
-	    else:
-	        torrent_id = torrent_id[0]['id']
+		event = request.GET.get('event', '')
+		if event not in ['started', 'completed', 'stopped'] and len(event.strip()) > 0:
+			raise Exception("event '%s' is invalid." % event)
 
-	    # calculate announce interval
-	    now = datetime.datetime.now()
+		numwant = request.GET.get('numwant', 50)
+		try:
+			numwant = int(numwant)
+		except ValueError:
+			numwant = 50
 
-	    mc = memcache.Client([MEMCACHE], debug=0)
-	    peers = mc.get('peers')
+		peers = []
+		if request.GET.get('compact'):
+			peers_l = ""
+			for peer in peers:
+				peers_l += struct.pack('>4sH', socket.inet_aton(peer['ip']), peer['port'])
 
-	    if not peers:
-	        peers = []
+		elif request.GET.get('no_peer_id'):
+			peers_l = []
+			for peer in peers:
+				peers_ni.append({'ip': peer['ip'], 'port': peer['port']})
 
-	    if not OPEN_TRACKER:
-	        dwns = len([p for p in peers if p['user_id'] == u.id and p['expire_time']>now])
-	        cur_dwns = u.attrs.get('max_sim_dwn', 2)
-	        if dwns >= cur_dwns and cur_dwns != 0:
-	            return _fail("maximum number of simultaneous downloads reached: %s"% dwns)
+		else:
+			peers_l = []
+			for peer in peers:
+				peers.append({'ip': peer['ip'], 'port': peer['port'], 'peer id': peer['peer_id']})
 
-	    num_peers = len([p for p in peers if p['expire_time']>now])
-	    announce_rate = len([p for p in peers if p['update_time']>now-datetime.timedelta(minutes=1)])
-
-	    announce_interval = max(num_peers * announce_rate / (MAX_ANNOUNCE_RATE**2) * 60, MIN_ANNOUNCE_INTERVAL)
-	    # calculate expiration time offset
-	    if event == 'stopped':
-	        expire_time = 0
-	    else:
-	        expire_time = announce_interval * EXPIRE_FACTOR
-
-	    for p in peers:
-	        if p['peer_id'] == args['peer_id']:
-	            peers.remove(p)
-	    if event == 'completed':
-	        topic = Topic.objects.filter(torrent__pk=torrent_id)
-	        if len(topic)>0:
-	            topic[0].attrs['downloaded'] = topic[0].attrs.get('downloaded', 0)+1
-	            topic[0].save()
-	    if event != 'stopped':
-	        peer_dict = {
-	            'info_hash': info_hash,
-	            'peer_id': args['peer_id'],
-	            'ip': args['ip'],
-	            'port': args['port'],
-	            'uploaded': args['uploaded'],
-	            'downloaded': args['downloaded'],
-	            'left': args['left'],
-	            'expire_time': now+datetime.timedelta(seconds=int(expire_time)),
-	            'update_time': now,
-	            'torrent_id': torrent_id,
-	        }
-	        if not OPEN_TRACKER:
-	            peer_dict['user_id'] = u.id
-	        peers.append(peer_dict)
-
-	    mc.set('peers', peers)
-
-	    numwant = request.GET.get('numwant', 50)
-	    try:
-	        numwant = int(numwant)
-	    except ValueError:
-	        numwant = 50
-	    result = [p for p in peers if p['torrent_id'] == torrent_id and p['expire_time']>now and p['info_hash']==info_hash] #this may be optimized
-	    shuffle(result)
-	    result = result[:numwant]
-
-	    if request.GET.get('compact'):
-	        peers = ""
-	        for peer in result:
-	            peers += pack('>4sH', inet_aton(peer['ip']), peer['port'])
-	    elif request.GET.get('no_peer_id'):
-	        peers = []
-	        for peer in result:
-	            peers.append({'ip': peer['ip'], 'port': peer['port']})
-	    else:
-	        peers = []
-	        for peer in result:
-	            peers.append({'ip': peer['ip'], 'port': peer['port'], 'peer id': peer['peer_id']})
-
-	    return HttpResponse(bencode({
-	            'interval': int(announce_interval),
-	            'peers': peers,
-	        }),
-	        mimetype = 'text/plain')
+		return HttpResponse(
+			bencode.bencode({
+				'interval': 60,
+				'peers': peers_l,
+			}),
+			mimetype = 'text/plain'
+		)
 
 	except:
-		response_fail(sys.exc_info()[0])
+		return response_fail(sys.exc_info()[1])
 
 
 def scrape(request):
-    #r = redis.StrictRedis(host='localhost')
-    raise Exception(request.GET)
-    return render_to_response('announce/scrape.html', {})
+	#r = redis.StrictRedis(host='localhost')
+	raise Exception(request.GET)
+	return render_to_response('announce/scrape.html', {})
 
 
 def response_fail(reason, xhr=False):
-    if not xhr:
-        return HttpResponse(benc.bencode({'failure reason': reason}))
+	raise Exception(reason)
+	if not xhr:
+		return HttpResponse(bencode.bencode({'failure reason': reason or 'unknown'}))
 
-    r = HttpResponse(mimetype="text/xml")
-    r.write("""<?xml version="1.0" encoding="UTF-8"?>""")
-    r.write("""<result><msg>failure</msg></result>""")
-    return r
+	r = HttpResponse(mimetype="text/xml")
+	r.write("""<?xml version="1.0" encoding="UTF-8"?>""")
+	r.write("""<result><msg>failure</msg></result>""")
+	return r
